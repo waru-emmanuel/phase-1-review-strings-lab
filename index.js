4798e223b8d1848071b4c9acd718f40cab457515
wachira <wewaruts@gmail.com>
2024-03-25 22:25:31 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = 'Chief Patrick';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = `Welcome, ${currentUser}!`;
+//const shortGreeting = 'Welcome, ';
+
+currentUser.slice(0, 1);
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
